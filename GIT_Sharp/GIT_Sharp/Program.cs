a96ab6762b2470003c9ad21a89dbda8905ee523a
VUser <nxnxngh@gmail.ocm>
2019-03-18 17:38:39 +0200

    Complited!

--- GIT_Sharp/GIT_Sharp/Program.cs
+++ GIT_Sharp/GIT_Sharp/Program.cs
@@ -4,18 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GIT_Sharp
+namespace Net_lesson1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n;
-            Console.WriteLine("Enter a number:");
-            n = Convert.ToInt32(Console.ReadLine());
-            n = Convert.ToInt32(Math.Pow(n,3));
-            Console.WriteLine("Result: " + n);
-            Console.ReadKey(true);
+            /*
+            Triangle t1 = new Triangle();
+
+            t1.SideA = 3;
+            t1.SideB = 4;
+            t1.SideC = 5;
+            Console.WriteLine(t1.ToString());
+            t1.PrintInfo();
+            
+            Triangle t2 = new Triangle(2, 6, 5, "T1");
+            t2.PrintInfo();
+            */
+
+            int k = 3;
+            Triangle[] triangles = new Triangle[k];
+            Random r = new Random();
+            double square = 0;
+            int realTriangles = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                triangles[i] = new Triangle(r.NextDouble() * 10, r.NextDouble() * 10, r.NextDouble() * 10, "T" + (i + 1).ToString());
+                Console.WriteLine(triangles[i].ToString());
+                if (triangles[i].IsTriangle())
+                {
+                    square += triangles[i].GetSquare();
+                    realTriangles++;
+                }
+            }
+
+            Console.WriteLine("Average square of " + realTriangles + " triangles is: " + square / realTriangles);
         }
     }
 }
---
+++ Lab2_CS/Lab2_CS/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Triangle triangle = new Triangle();
+            Triangle.RightTriangle rightTriangle = new Triangle.RightTriangle();
+
+            triangle.Input();
+            triangle.CoordA[0] = Convert.ToInt32(textBox1.Text);
+            triangle.CoordA[1] = Convert.ToInt32(textBox2.Text);
+            triangle.CoordB[0] = Convert.ToInt32(textBox4.Text);
+            triangle.CoordB[1] = Convert.ToInt32(textBox3.Text);
+            triangle.CoordC[0] = Convert.ToInt32(textBox5.Text);
+            triangle.CoordC[1] = Convert.ToInt32(textBox6.Text);
+            rightTriangle.Input();
+            rightTriangle.CoordA[0] = Convert.ToInt32(textBox1.Text);
+            rightTriangle.CoordA[1] = Convert.ToInt32(textBox2.Text);
+            rightTriangle.CoordB[0] = Convert.ToInt32(textBox4.Text);
+            rightTriangle.CoordB[1] = Convert.ToInt32(textBox3.Text);
+            rightTriangle.CoordC[0] = Convert.ToInt32(textBox5.Text);
+            rightTriangle.CoordC[1] = Convert.ToInt32(textBox6.Text);
+
+            //https://metanit.com/sharp/windowsforms/4.19.php
+
+            //rightTriangle.Input();
+            triangle.Check();
+            //rightTriangle.Check();
+            triangle.Output();
+            //rightTriangle.Output();
+            richTextBox1.Text = triangle.Output();
+            triangle.CoordA[0] = Convert.ToInt32(textBox1.Text);
+        }
+    }
+}
