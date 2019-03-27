2e32d67caafef26d66ec16c82cb9cd9fd9422fa8
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2019-03-27 09:58:03 +0200

    Update Triangle.cs

--- Lab2_CS/Lab2_CS/Triangle.cs
+++ Lab2_CS/Lab2_CS/Triangle.cs
@@ -1,60 +1,56 @@
-﻿using System;
+using System;
 
 namespace Lab2_CS
 {
-    internal class Triangle
+    public class Triangle
     {
-            public int[] CoordA = new int[2];
+        public int[] CoordA = new int[2];
+        public int[] CoordB = new int[2];
+        public int[] CoordC = new int[2];
 
-            public int[] CoordB = new int[2];
+        private double stringA;
+        private double stringB;
+        private double stringC;
 
-            public int[] CoordC = new int[2];
+        private double area;
 
-            public double StringA;
+        public double StringA { get => stringA;}
+        public double StringB { get => stringB;}
+        public double StringC { get => stringC;}
+        public double Area { get => area;}
 
-            public double StringB;
+        public void CalculateSides()
+        {
+            stringA = (Math.Sqrt(Math.Pow((CoordB[0] - CoordA[0]), 2) + (Math.Pow(CoordB[1] - CoordA[0], 2))));
+            stringB = (Math.Sqrt(Math.Pow((CoordC[0] - CoordB[0]), 2) + (Math.Pow(CoordC[1] - CoordB[0], 2))));
+            stringC = (Math.Sqrt(Math.Pow((CoordA[0] - CoordC[0]), 2) + (Math.Pow(CoordA[1] - CoordC[0], 2))));
+        }
 
-            public double StringC;
+        public void CalculateArea()
+        {
+            area = StringA * StringB * StringC;
+        }
 
-            public double Area;
+        public bool IsTriangle()
+        {
+            this.CalculateSides();
 
-            public virtual string Check()
-            {
-            StringA = (Math.Sqrt(Math.Pow((CoordB[0] - CoordA[0]), 2) + (Math.Pow(CoordB[1] - CoordA[0],2))));
-
-            StringB = (Math.Sqrt(Math.Pow((CoordC[0] - CoordB[0]), 2) + (Math.Pow(CoordC[1] - CoordB[0],2))));
-
-            StringC = (Math.Sqrt(Math.Pow((CoordA[0] - CoordC[0]), 2) + (Math.Pow(CoordA[1] - CoordC[0], 2))));
             if (StringA + StringB <= StringC || StringB + StringC <= StringA || StringA + StringC <= StringB)
-                {
-                   return("This triangle is impossible\n");
+                return false;
+            else
+                return true;
+        }
 
-                }
-                   return "";
-              }
-            public string Output()
-            {
-            Area = (StringA * StringB * StringC);
+        override public string ToString()
+        {
             return ("Length of string A = " + StringA +
                 "\nLength of string B = " + StringB +
                 "\nLength of string C = " + StringC +
                 "\nA Angle’s value = " + (Math.Acos((StringB * StringB + StringC * StringC - StringA * StringA) / (2 * StringB * StringC)) * 180 / Math.PI) + " degrees" +
-                "\nB Angle’s value = " + (Math.Acos((StringA * StringA + StringC * StringC - StringB * StringB) / (2 * StringA * StringC)) * 180 / Math.PI) + " degrees" + 
-                "\nC Angle’s value = " + ((Math.Acos((StringB * StringB + StringA * StringA - StringC * StringC) / (2 * StringB * StringA)) * 180) / Math.PI) + " degrees" + 
+                "\nB Angle’s value = " + (Math.Acos((StringA * StringA + StringC * StringC - StringB * StringB) / (2 * StringA * StringC)) * 180 / Math.PI) + " degrees" +
+                "\nC Angle’s value = " + ((Math.Acos((StringB * StringB + StringA * StringA - StringC * StringC) / (2 * StringB * StringA)) * 180) / Math.PI) + " degrees" +
                 "\nPerimeter of triangle = " + (StringA + StringB + StringC) +
                 "\nArea of triangle = " + Area);
-             }
-        public class RightTriangle : Triangle
-       {
-                public override string Check()
-                {
-                    base.Check();
-                    if (StringA * StringA + StringB * StringB == StringC * StringC || StringC * StringC + StringB * StringB == StringA * StringA || StringA * StringA + StringC * StringC == StringB * StringB)
-                    {
-                        return ("This triangle is right");
-                    }
-                 return "";
-                }
         }
     }
 }
